2e89a36fdb5858ec294ded133c4832cb96d02d5a
TazakiN <tazkia.nizami@gmail.com>
2025-08-20 13:53:14 +0700

    post-2

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,30 @@
 
 use core::panic::PanicInfo;
 
+static MESSAGE: &[u8] = b"Fiat justitia ruat caelum";
+
 #[unsafe(no_mangle)] // don't mangle the name of this function (mencegah namanya ditambah tambahin random, biar namanya tetep _start)
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8; // memori VGA Buffer, write langsung ke 0xb8000
+    let screen_width = 80;
+    let screen_height = 25;
+    let message_len = MESSAGE.len();
+
+    // Center text
+    let row = screen_height / 2;
+    let col = (screen_width - message_len) / 2;
+    let start_offset = (row * screen_width + col) * 2;
+
+    let attribute = 0x0fu8;
+
+    for (i, &byte) in MESSAGE.iter().enumerate() {
+        unsafe {
+            // unsafe : kasih tau rust kalo write disini (alamat vga_buffer) aman
+            *vga_buffer.offset((start_offset + i * 2) as isize) = byte;
+            *vga_buffer.offset((start_offset + i * 2 + 1) as isize) = attribute;
+        }
+    }
+
     loop {
     }
 }
