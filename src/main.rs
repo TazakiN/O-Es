ecc6f773d2a00697e0c2ecb31d316f69187a2cea
TazakiN <tazkia.nizami@gmail.com>
2025-10-05 23:57:08 +0700

    post-5: cpu exception

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,15 @@ use blog_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    blog_os::init();
+
+    // test breakpoint
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It didn't crash!");
     loop {
     }
 }
