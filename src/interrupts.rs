795aed3eaefbf2706dde58b51ba16ea16183008c
TazakiN <tazkia.nizami@gmail.com>
2025-10-17 22:02:53 +0700

    post-8: paging-intro

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,6 @@
 use spin::Mutex;
-use x86_64::structures::idt::{ InterruptDescriptorTable, InterruptStackFrame };
-use crate::{ gdt, print, println };
+use x86_64::structures::idt::{ InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode };
+use crate::{ gdt, hlt_loop, print, println };
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 
@@ -22,6 +22,7 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
@@ -65,6 +66,19 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
     unsafe {
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
     }
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: PageFaultErrorCode
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:#x}", Cr2::read());
+    println!("Error Code: {:#x}", _error_code);
+    println!("Stack Frame: {:#?}", stack_frame);
+    hlt_loop();
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
